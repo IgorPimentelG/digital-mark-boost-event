aa1ecc1d475b3217fd7484ef4e76d505eddb9206
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-11-20 17:19:14 -0300

    :sparkles: feat: add event service

--- boost-event-api/src/main/java/com/digital/mark/boost/event/infra/repositories/EventRepository.java
+++ boost-event-api/src/main/java/com/digital/mark/boost/event/infra/repositories/EventRepository.java
@@ -2,7 +2,14 @@ package com.digital.mark.boost.event.infra.repositories;
 
 import com.digital.mark.boost.event.domain.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface EventRepository extends JpaRepository<Event, String> {}
+public interface EventRepository extends JpaRepository<Event, String> {
+	@Query("SELECT e FROM Event e WHERE e.id = :id AND e.user.id = :userId")
+	Optional<Event> findById(@Param("id") String id, @Param("userId") String userId);
+}
